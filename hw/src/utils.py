9438ba6b10a439a58d5f05440c75e89e3d4c10d4
Joshua Joseph <joshjoseph97@gmail.com>
2024-01-15 20:30:17 -0500

    :truck: move and merge out-of-place utils file
    Co-authored-by: Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>

--- hw/src/utils.py
+++ hw/src/utils.py
@@ -1,10 +1,21 @@
-import ast
+import fileinput, re, ast
+
+# Reference: https://discord.com/channels/1191838787219759154/1192507528882438247/1195863830136377345
+# Returns the values in the next row of the CSV as a list along with the row number of the next
+def csv(filename="-"):
+    i = 0
+    with fileinput.FileInput(None if filename=="-" else filename) as src:
+        for line in src:
+            line = re.sub(r'([\n\t\r"\' ]|#.*)', '', line)
+            if line:
+                i += 1
+                yield i, [coerce(x) for x in line.split(",")]
 
 def output(x):
     class_name = x.__class__.__name__
     items = ", ".join([f"{k}:{v}" for k, v in x.items() if k[0] != "_"])
     return f"{class_name} {{{items}}}"
 
-def coerce(x):
-   try: return ast.literal_eval(x)
-   except Exception: return x.strip()
+def coerce(s):
+   try: return ast.literal_eval(s)
+   except Exception: return s.strip()
