fbe11254dc6ba099facbe24ac78d3217ffbb3c51
Jacob Gerlach <jwgerlach00@gmail.com>
2024-01-16 10:51:59 -0500

    Added some comments

--- hw/src/test.py
+++ hw/src/test.py
@@ -7,13 +7,19 @@ class Test:
         self.config = config
 
     def stats(self):
+        # Test stats correct
         stats = output(Data(self.config.file).stats())
         print(stats)
         return stats == "{.N: 398, Acc+: 15.57, Lbs-: 2970.42, Mpg+: 23.84}"
 
     def test_coerce(self):
+        # Test string is converted to python dictionary
         dict_ex = coerce("{'a': 1, 'b': 2}")
+        print(dict_ex)
         return type(dict_ex) == dict
 
     def test_output(self):
-        return output({"a": 1, "b": 2}) == "{a: 1, b: 2}"
+        # Test ' is removed from dict for string representation
+        out = output({"a": 1, "b": 2})
+        print(out)
+        return out == "{a: 1, b: 2}"
