1f74700e8c456159bb89e3178ffaf1e00a0fd73a
Jacob Gerlach <jwgerlach00@gmail.com>
2024-01-16 10:47:22 -0500

    Fixed formatting for black

--- hw/src/test.py
+++ hw/src/test.py
@@ -10,10 +10,10 @@ class Test:
         stats = output(Data(self.config.file).stats())
         print(stats)
         return stats == "{.N: 398, Acc+: 15.57, Lbs-: 2970.42, Mpg+: 23.84}"
-    
+
     def test_coerce(self):
         dict_ex = coerce("{'a': 1, 'b': 2}")
         return type(dict_ex) == dict
 
     def test_output(self):
-        return output({'a': 1, 'b': 2}) == "{a: 1, b: 2}"
+        return output({"a": 1, "b": 2}) == "{a: 1, b: 2}"
