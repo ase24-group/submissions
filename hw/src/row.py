ff342f50f5aa398efb71bad7c0075d273ef03806
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-01-21 18:51:11 -0500

    Implement the likes method for the Row class

--- hw/src/row.py
+++ hw/src/row.py
@@ -1,3 +1,17 @@
+from config import get_config
+
 class Row:
     def __init__(self, t):
         self.cells = t
+
+    def likes(self, datas):
+        n, nHypothesis = 0, 0
+        most, out = None, None
+        for data in datas.values():
+            n += len(data.rows)
+            nHypothesis += 1
+        for k, data in datas.items():
+            tmp = self.like(data, n, nHypothesis)
+            if (most is None) or (tmp > most):
+                most, out = tmp, k
+        return out, most
