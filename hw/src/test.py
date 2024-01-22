12cd33b812b61b32be8e01c9890c333e87480e50
Joshua Joseph <joshjoseph97@gmail.com>
2024-01-22 18:45:55 -0500

    :recycle: `test_` prefix under the `Test` class is redundant

--- hw/src/test.py
+++ hw/src/test.py
@@ -12,19 +12,19 @@ class Test:
         print(stats)
         return stats == "{.N: 398, Acc+: 15.57, Lbs-: 2970.42, Mpg+: 23.84}"
 
-    def test_coerce(self):
+    def coerce(self):
         # Test string is converted to python dictionary
         dict_ex = coerce("{'a': 1, 'b': 2}")
         print(dict_ex)
         return type(dict_ex) == dict
 
-    def test_output(self):
+    def output(self):
         # Test ' is removed from dict for string representation
         out = output({"a": 1, "b": 2})
         print(out)
         return out == "{a: 1, b: 2}"
 
-    def test_count_classes(self) -> None:
+    def count_classes(self) -> None:
         data_obj = Data(self.config.file)
         class_counts = data_obj.cols.klass.has
 
