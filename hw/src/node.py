b11f185970d48416b8fb7da19316511bf76fc200
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-02-19 17:06:33 -0500

    Implemented NODE.show

--- hw/src/node.py
+++ hw/src/node.py
@@ -1,6 +1,15 @@
+import utils
+import math
+
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, left=None, right=None, C=None, cut=None, lefts=None, rights=None):
         self.here = data
+        self.left = left
+        self.right = right
+        self.C = C
+        self.cut = cut
+        self.lefts = lefts
+        self.rights = rights
 
     # Walks over the tree and calls 'fun' on each node of the tree
     def walk(self, fun, depth=0):
@@ -9,5 +18,18 @@ class Node:
             self.lefts.walk(fun, depth+1)
         if self.rights:
             self.rights.walk(fun, depth+1)
+        
+    # Prints a tree by printing each node in order
+    def show(self):
+        def d2h(data):
+            return round(data.mid().d2h(self.here))
+        max_depth = 0
 
+        def _show(node, depth, leafp):
+            post = (str(d2h(node.here)) + "\t" + utils.output(node.here.mid().cells)) if leafp else ""
+            max_depth = math.max(max_depth, depth)
+            print(('|..')*depth + post)
 
+        self.walk(_show)
+        print(("    ")*max_depth + str(d2h(self.here)) + utils.output(self.here.mid().cells))
+        print(("    ")*max_depth + "_" + utils.output(self.here.cols.names))
