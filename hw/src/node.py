a209b9249fd389755fa7d7e8940a6548a51418a6
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-02-19 15:56:04 -0500

    Implemented NODE.walk

--- hw/src/node.py
+++ hw/src/node.py
@@ -2,4 +2,12 @@ class Node:
     def __init__(self, data):
         self.here = data
 
+    # Walks over the tree and calls 'fun' on each node of the tree
+    def walk(self, fun, depth=0):
+        fun(self, depth, not (self.lefts or self.rights))
+        if self.lefts:
+            self.lefts.walk(fun, depth+1)
+        if self.rights:
+            self.rights.walk(fun, depth+1)
+
 
